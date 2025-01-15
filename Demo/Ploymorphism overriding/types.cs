2a33167741326a238fd1c4f0718387d0a904db5b
Omar <Omar@DESKTOP-OUKQKJG>
2025-01-15 23:35:26 +0300

    ass3 and demo in demo folder

---
+++ Demo/Ploymorphism overriding/types.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism.Ploymorphism_overriding
+{
+    class typeA
+    {
+        public int A { get; set; }
+        public typeA(int A)
+        {
+            this.A = A;
+        }
+
+
+        public void Myfunc01()
+        {
+            Console.WriteLine("Iam Base [Parent]");
+        }
+        public virtual void MyFun02()
+        {
+            Console.WriteLine($"Type : a = {A}");
+        }
+
+    }
+
+    class TypeB : typeA
+    {
+        public int B { get; set; }
+        public TypeB(int A, int B) : base(A)
+        {
+            this.B=B;
+        }
+
+
+        public new void  Myfunc01()
+        {
+            Console.WriteLine($"My func01 =>I am Derived [child] ");
+        }
+
+        public override void MyFun02()
+        {
+            Console.WriteLine($"my fun02 =>TypeB : A {A}, B= {B}");
+        }
+
+
+    }
+}
---
+++ Demo/casting operators overloading/userviewmodel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism.casting_operators_overloading
+{
+    internal class userviewmodel
+    {
+        public int Id { get; set; }
+        public string? Fname { get; set; }
+        public string? LName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+
+        public static explicit operator userviewmodel(user user)
+        {
+
+            string[]? Names = user.FullName?.Split(' ');
+            return new userviewmodel()
+            {
+                Id = user?.Id ?? 0,
+                Fname = Names?.Length > 0 ? Names[0] : string.Empty,
+                LName = Names?.Length > 1 ? Names[1] : string.Empty,
+                Email = user?.Email ?? string.Empty,
+                Password = user?.Password ?? string.Empty,
+
+
+            };
+        }
+    }
+}
